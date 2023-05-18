e9c6a10ca617c7e1347b051555de196981938fb0
zeneto11 <josealmeidaneto2002@gmail.com>
2023-05-18 00:25:50 -0400

    Aula 16/05

--- aula16_05/cod1-basico.py
+++ aula16_05/cod1-basico.py
@@ -1,6 +1,6 @@
 import cv2
 
-imagem = cv2.imread('wallpaper_hypnotize_IA.jpg') # obter a imagem
+imagem = cv2.imread('aula16_05/images/wallpaper_hypnotize_IA.jpg') # obter a imagem
 
 cv2.imshow('Nome da janela', imagem) # exibir imagem em uma nova janela
 cv2.waitKey(0) # faz com que a imagem fique sendo mostrada
@@ -9,4 +9,4 @@ print(imagem.shape[0]) # altura da imagem
 print(imagem.shape[1]) # largura da imagem
 print(imagem.shape[2]) # quantidade de canais da imagem
 
-cv2.imwrite('novo_arquivo.jpg', imagem) # exportar arquivo da imagem
+cv2.imwrite('aula16_05/images/novo_arquivo.jpg', imagem) # exportar arquivo da imagem
