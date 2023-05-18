9c13d214cac08ec75cf851a489ec13d0e9ef556d
zeneto11 <josealmeidaneto2002@gmail.com>
2023-05-18 19:04:03 -0400

    Tarefa Zebra

--- aula16_05/tarefa-zebra.py
+++ aula16_05/tarefa-zebra.py
@@ -1,9 +1,14 @@
 import cv2
 
-imagem = cv2.imread('zebra.jpg') # obter a imagem
+imagem = cv2.imread('aula16_05/images/zebra.jpg') # obter a imagem
+
+for y in range(imagem.shape[0]):
+    for x in range(imagem.shape[1]):
+        (b, g, r) = imagem[y, x]
+        if (b > 200) and (g > 200) and (r > 200):
+            imagem[y, x] = (0, 0, 255)
 
 cv2.imshow('Zebra dos cria', imagem) # exibir imagem em uma nova janela
 cv2.waitKey(0) # faz com que a imagem fique sendo mostrada
 
-
-cv2.imwrite('zebra-flamenguista.jpg', imagem) # exportar arquivo da imagem
+cv2.imwrite('aula16_05/images/zebra-flamenguista.jpg', imagem) # exportar arquivo da imagem
---
+++ aula17_05/fundo_e_desenhos.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+canvas = np.ones((300, 400, 3)) * 255 # faz um fundo branco
+
+# desenhar linha azul diagonal
+azul = (255, 0, 0)
+cv2.line(canvas, (0,0), (400,300), azul)
+
+# desenhar linha azul diagonal
+verde = (0, 255, 0)
+cv2.line(canvas, (200,0), (200,300), verde, 3)
+
+# desenhar retângulo com borda verde
+cv2.rectangle(canvas, (10,70), (90,190), verde)
+
+# desenhar retângulo todo vermelho
+vermelho = (0, 0, 255)
+cv2.rectangle(canvas, (250,50), (300,125), vermelho, -1)
+
+# desenhar círculo
+preto = (0, 0, 0)
+cv2.circle(canvas, (130,230), 50, preto)
+
+cv2.imshow('Canvas', canvas) # exibe a imagem
+cv2.waitKey(0)
